4a2f348b176ea0097889cd0c25c682e366b7beba
marloestacx <marloes.tacx@live.nl>
2021-02-10 14:59:58 +0100

    Serve

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Express web app on localhost:3000');
 });
+
+app.get('/login', (req, res) => {
+  res.send('Login pagina')
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send("Deze pagin kan niet gevonden worden!")
+});
