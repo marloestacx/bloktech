c16a76c6b6284c422ab19b642ac70ab7e092bd33
marloestacx <marloes.tacx@live.nl>
2021-02-12 17:22:19 +0100

    handlebars geinstalleerd

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const app = express();
+const exphbs  = require('express-handlebars');
+
+const profile = [
+  {name: "Piet", age: 25, description: "Houdt van voetbal"},
+  {name: "Jan", age: 54, description: "Houdt van gamen"},
+  {name: "Klaas", age: 23, description: "Houdt van sporten"},
+]
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('home',{});
+});
+
+app.get('/login', (req, res) => {
+  res.send('Login pagina');
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', {title: "Profiel", profile})
+})
+
+app.use(function (req, res, next) {
+  res.status(404).send("Deze pagin kan niet gevonden worden!");
 });
 
 app.listen(3000, () => {
   console.log('Express web app on localhost:3000');
-});
-
-app.get('/login', (req, res) => {
-  res.send('Login pagina')
-});
-
-app.use(function (req, res, next) {
-  res.status(404).send("Deze pagin kan niet gevonden worden!")
 });
