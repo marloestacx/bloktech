8d8b383d2eea55a6f28b9ea9a7b5891d42c18452
marloestacx <marloes.tacx@live.nl>
2021-03-07 16:47:11 +0100

    Style aanpassingen

--- index.js
+++ index.js
@@ -6,18 +6,16 @@ const slug = require('slug');
 const MongoClient = require('mongodb').MongoClient;
 const dotenv = require('dotenv').config()
 
-
-const geslacht = ["man","vrouw", "allebei"];
+const geslacht = ["man","vrouw"];
 const leeftijd = ["20-30", "30-40", "40-50", "50+"];
 
 //alles nl of engels
 
 let db = null;
-// function connectDB
 async function connectDB () {
-  // get URI from .env file
+  // URI van .env bestand
   const uri = process.env.DB_URI
-  // make connection to database
+  // connectie met database
   const options = { useUnifiedTopology: true };
   const client = new MongoClient(uri, options)
   await client.connect();
@@ -25,11 +23,11 @@ async function connectDB () {
 }
 connectDB()
   .then(() => {
-    // if succesfull connections is made, show a message
+    // als er verbinding is
     console.log('We have a connection to Mongo!')
   })
   .catch( error => {
-    // if connnection is unsuccesful, show errors
+    // als er geen verbinding is
     console.log(error)
   });
 
@@ -39,70 +37,31 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-// app.get('/', (req, res) => {
-//   res.render('home',{});
-// });
-
 app.get('/', async (req, res) => {
-  // create an empty list of profiles
   let profielen = {}
-  // look for alle profielen in database and sort them by name into an array
 
+  //haalt je voorkeur uit de database
   db.collection('voorkeur').findOne({}, async function(err, result) {
     if (err) throw err;
-    console.log(result.geslacht);
 
-  // var query = { geslacht: result.geslacht};  
+  var filter = {geslacht: result.geslacht, leeftijdcategory: result.leeftijd}; 
 
-  // look for alle profielen in database and sort them by name into an array
-  var query = { geslacht: result.geslacht, leeftijdcategory: result.leeftijd};  
-  
-  // console.log(db.collection('preference').find(range).toArray());
-  profielen = await db.collection('profielen').find(query).toArray();
-  res.render('home', {title:'List of all profielen', profielen})
-  console.log(query);
+  // haalt alle profielen uit de database op en stopt ze in een array
+  profielen = await db.collection('profielen').find(filter).toArray();
+  res.render('home', {profielen})
   });
 });
 
 app.get('/filter', (req, res) => {
-  // let voorkeur = {}
-  // voorkeur =  db.collection('preference').find({},{sort: {geslacht: 1}}).toArray();
   res.render('filter',{geslacht, leeftijd});
 });
 
 app.post('/filter', async (req,res) => {
   const id = slug(req.body.geslacht);
-  // const voorkeur = {"id": "id", "geslacht": req.body.geslacht, "leeftijd": req.body.leeftijd};
-  // await db.collection('voorkeur').insertOne(voorkeur);
 
-  await db.collection("voorkeur")
-        .findOneAndUpdate(
-            { id: "id" },
-            { $set: {"geslacht": req.body.geslacht, "leeftijd": req.body.leeftijd }},
-            { new: true, upsert: true, returnOriginal: false })
-
-  res.render('home', {title: "Added a new movie"})
+  await db.collection("voorkeur").findOneAndUpdate({ id: "id" },{ $set: {"geslacht": req.body.geslacht, "leeftijd": req.body.leeftijd }},{ new: true, upsert: true, returnOriginal: false })
+  res.redirect('/')
 });
-
-// app.get('/profiles', (req, res) => {
-//   res.render('profiles', {title: "Profiel", profiles})
-// })
-
-// app.get('/profiles/add', (req, res) => {
-//   res.render('add', {title: "Add profile"});
-// });
-
-// app.post('/profiles/add', (req,res) => {
-//   const id = slug(req.body.name);
-//   const profile = {"id": "id", "name": req.body.name, "age": req.body.age, "description": req.body.description};
-//   profiles.push(profile);
-//   res.render('profiledetails', {title: "Added a new profile", profile})
-// });
-
-// app.get('/profiles/:profileId', (req, res) => {
-//   const profile = profiles.find( profile => profile.id == req.params.profileId);
-//   res.render('profiledetails', {title: "Profile details", profile})
-// });
 
 app.use(function (req, res, next) {
   res.status(404).send("Deze pagin kan niet gevonden worden!");
